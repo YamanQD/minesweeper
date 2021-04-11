3f33c305891c37e6ff3b1089fd54603184186440
Yaman Qassas <yaman102011@gmail.com>
2021-04-11 21:21:52 +0300

    count mines left

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-import { createBoard, revealTile, markTile } from './logic.js';
+import { createBoard, revealTile, markTile, TILE_STATUSES } from './logic.js';
 var BOARD_SIZE = 3, MINE_COUNT = 4;
 var board = createBoard(BOARD_SIZE, MINE_COUNT);
 var boardElement = document.querySelector('.board');
+var minesLeftText = document.querySelector('[data-mines-left]');
+minesLeftText.textContent = MINE_COUNT + "";
 boardElement.style.setProperty("--size", BOARD_SIZE + "");
 board.forEach(function (row) {
     row.forEach(function (tile) {
@@ -12,6 +14,13 @@ board.forEach(function (row) {
         tile.element.addEventListener('contextmenu', function (e) {
             e.preventDefault();
             markTile(tile);
+            countMinesLeft();
         });
     });
 });
+var countMinesLeft = function () {
+    var markedMines = board.reduce(function (count, row) {
+        return count + row.filter(function (tile) { return tile.status === TILE_STATUSES.MARKED; }).length;
+    }, 0);
+    minesLeftText.textContent = MINE_COUNT - markedMines + "";
+};
--- ts/script.ts
+++ ts/script.ts
@@ -1,10 +1,13 @@
-import {createBoard, revealTile, markTile} from './logic.js'
+import {createBoard, revealTile, markTile, TILE_STATUSES} from './logic.js'
 
 const BOARD_SIZE = 3, MINE_COUNT = 4
 
 const board = createBoard(BOARD_SIZE, MINE_COUNT)
 
 const boardElement = <HTMLElement> document.querySelector('.board')
+const minesLeftText = <HTMLElement> document.querySelector('[data-mines-left]') 
+minesLeftText.textContent = MINE_COUNT + ""
+
 boardElement.style.setProperty("--size", BOARD_SIZE + "")
 
 board.forEach(row => {
@@ -17,6 +20,15 @@ board.forEach(row => {
         tile.element.addEventListener('contextmenu', (e) => {
             e.preventDefault()
             markTile(tile)
+            countMinesLeft()
         })
     })
 })
+
+const countMinesLeft = (): void => {
+    const markedMines = board.reduce((count, row) => {
+        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+    }, 0)
+
+    minesLeftText.textContent =  MINE_COUNT - markedMines + ""
+}

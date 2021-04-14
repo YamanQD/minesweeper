4fc3d92b85acca23ce88f9645878cb1efe42640a
Yaman Qassas <yaman102011@gmail.com>
2021-04-15 02:50:14 +0300

    add replay functionality

--- js/script.js
+++ js/script.js
@@ -4,83 +4,97 @@ var board = createBoard(BOARD_SIZE, MINE_COUNT);
 var boardElement = document.querySelector('.board');
 var gameEndText = document.querySelector('.game-end');
 var minesLeftText = document.querySelector('[data-mines-left]');
-minesLeftText.textContent = MINE_COUNT + "";
-boardElement.style.setProperty("--size", BOARD_SIZE + "");
-board.forEach(function (row) {
-    row.forEach(function (tile) {
-        boardElement === null || boardElement === void 0 ? void 0 : boardElement.append(tile.element);
-        tile.element.addEventListener('click', function () {
-            revealTile(board, tile);
-            checkGameEnd();
-        });
-        tile.element.addEventListener('contextmenu', function (e) {
-            e.preventDefault();
-            markTile(tile);
-            countMinesLeft();
-        });
-    });
+var replayBtn = document.querySelector('.replay-btn');
+replayBtn === null || replayBtn === void 0 ? void 0 : replayBtn.addEventListener('click', function () {
+    gameEndText.textContent = '';
+    play();
 });
-var countMinesLeft = function () {
-    var markedMines = board.reduce(function (count, row) {
-        return count + row.filter(function (tile) { return tile.status === TILE_STATUSES.MARKED; }).length;
-    }, 0);
-    minesLeftText.textContent = MINE_COUNT - markedMines + "";
-};
-var checkGameEnd = function () {
-    var win = checkWin();
-    var lose = checkLose();
-    if (lose || win) {
-        boardElement.addEventListener("click", stopProp, { capture: true });
-        boardElement.addEventListener("contextmenu", stopProp, { capture: true });
-    }
-    if (lose) {
-        gameEndText.textContent = "YOU LOSE!";
-        board.forEach(function (row) {
-            row.forEach(function (tile) {
-                if (tile.status === TILE_STATUSES.MARKED) {
-                    if (tile.mine) {
-                        tile.element.style.setProperty('background-color', '#F88F32');
-                    }
-                    else {
-                        // tile.element.style.setProperty('background-color', '#1AD927')
-                        tile.element.className = 'crossed-flag-container';
-                        var cross = document.createElement('div');
-                        cross.textContent = 'X';
-                        cross.className = 'cross';
-                        tile.element.removeChild(tile.element.firstChild);
-                        tile.element.appendChild(cross);
-                    }
-                }
-                else if (tile.mine) {
-                    var img = document.createElement('img');
-                    img.src = './assets/mine.png';
-                    img.setAttribute('draggable', 'false');
-                    img.className = 'mine';
-                    tile.element.appendChild(img);
-                    tile.status = TILE_STATUSES.MINE;
-                }
+var play = function () {
+    minesLeftText.textContent = MINE_COUNT + "";
+    boardElement.innerHTML = '';
+    boardElement.style.setProperty("--size", BOARD_SIZE + "");
+    board = createBoard(BOARD_SIZE, MINE_COUNT);
+    board.forEach(function (row) {
+        row.forEach(function (tile) {
+            boardElement === null || boardElement === void 0 ? void 0 : boardElement.append(tile.element);
+            tile.element.addEventListener('click', function () {
+                revealTile(board, tile);
+                checkGameEnd();
+            });
+            tile.element.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+                markTile(tile);
+                countMinesLeft();
             });
         });
-    }
-    else if (win) {
-        gameEndText.textContent = "YOU WIN!";
-    }
-};
-var checkLose = function () {
-    return board.some(function (row) {
-        return row.some(function (tile) {
-            return tile.status === TILE_STATUSES.MINE;
-        });
     });
-};
-var checkWin = function () {
-    var revealedCount = board.reduce(function (count, row) {
-        return count + row.reduce(function (count, tile) {
-            return count + (tile.status === TILE_STATUSES.NUMBER ? 1 : 0);
+    var countMinesLeft = function () {
+        var markedMines = board.reduce(function (count, row) {
+            return count + row.filter(function (tile) { return tile.status === TILE_STATUSES.MARKED; }).length;
         }, 0);
-    }, 0);
-    return revealedCount === BOARD_SIZE * BOARD_SIZE - MINE_COUNT;
+        minesLeftText.textContent = MINE_COUNT - markedMines + "";
+    };
+    var checkGameEnd = function () {
+        var win = checkWin();
+        var lose = checkLose();
+        if (lose || win) {
+            board.forEach(function (row) {
+                row.forEach(function (tile) {
+                    tile.element.addEventListener('click', stopProp, { capture: true });
+                    tile.element.addEventListener('contextmenu', stopProp, { capture: true });
+                });
+            });
+        }
+        if (lose) {
+            gameEndText.textContent = "YOU LOSE!";
+            board.forEach(function (row) {
+                row.forEach(function (tile) {
+                    if (tile.status === TILE_STATUSES.MARKED) {
+                        if (tile.mine) {
+                            tile.element.style.setProperty('background-color', '#F88F32');
+                        }
+                        else {
+                            // tile.element.style.setProperty('background-color', '#1AD927')
+                            tile.element.className = 'crossed-flag-container';
+                            var cross = document.createElement('div');
+                            cross.textContent = 'X';
+                            cross.className = 'cross';
+                            tile.element.removeChild(tile.element.firstChild);
+                            tile.element.appendChild(cross);
+                        }
+                    }
+                    else if (tile.mine) {
+                        var img = document.createElement('img');
+                        img.src = './assets/mine.png';
+                        img.setAttribute('draggable', 'false');
+                        img.className = 'mine';
+                        tile.element.appendChild(img);
+                        tile.status = TILE_STATUSES.MINE;
+                    }
+                });
+            });
+        }
+        else if (win) {
+            gameEndText.textContent = "YOU WIN!";
+        }
+    };
+    var checkLose = function () {
+        return board.some(function (row) {
+            return row.some(function (tile) {
+                return tile.status === TILE_STATUSES.MINE;
+            });
+        });
+    };
+    var checkWin = function () {
+        var revealedCount = board.reduce(function (count, row) {
+            return count + row.reduce(function (count, tile) {
+                return count + (tile.status === TILE_STATUSES.NUMBER ? 1 : 0);
+            }, 0);
+        }, 0);
+        return revealedCount === BOARD_SIZE * BOARD_SIZE - MINE_COUNT;
+    };
+    var stopProp = function (e) {
+        e.stopImmediatePropagation();
+    };
 };
-var stopProp = function (e) {
-    e.stopImmediatePropagation();
-};
+play();
--- ts/script.ts
+++ ts/script.ts
@@ -2,96 +2,115 @@ import { createBoard, revealTile, markTile, TILE_STATUSES } from './logic.js'
 
 const BOARD_SIZE = 9, MINE_COUNT = 16
 
-const board = createBoard(BOARD_SIZE, MINE_COUNT)
-const boardElement = <HTMLElement> document.querySelector('.board')
-const gameEndText = <HTMLElement> document.querySelector('.game-end')
-const minesLeftText = <HTMLElement> document.querySelector('[data-mines-left]')
-minesLeftText.textContent = MINE_COUNT + ""
+let board = createBoard(BOARD_SIZE, MINE_COUNT)
+const boardElement = <HTMLElement>document.querySelector('.board')
+const gameEndText = <HTMLElement>document.querySelector('.game-end')
+const minesLeftText = <HTMLElement>document.querySelector('[data-mines-left]')
+const replayBtn = document.querySelector('.replay-btn')
 
-boardElement.style.setProperty("--size", BOARD_SIZE + "")
+replayBtn?.addEventListener('click', () => {
+    gameEndText.textContent = ''
 
-board.forEach(row => {
-    row.forEach(tile => {
-        boardElement?.append(tile.element)
-
-        tile.element.addEventListener('click', () => {
-            revealTile(board, tile)
-            checkGameEnd()
-        })
-        tile.element.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
-            markTile(tile)
-            countMinesLeft()
-        })
-    })
+    play()
 })
 
-const countMinesLeft = (): void => {
-    const markedMines = board.reduce((count, row) => {
-        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
-    }, 0)
+const play = () => {
+    minesLeftText.textContent = MINE_COUNT + ""
 
-    minesLeftText.textContent = MINE_COUNT - markedMines + ""
-}
+    boardElement.innerHTML = ''
+    boardElement.style.setProperty("--size", BOARD_SIZE + "")
 
-const checkGameEnd = (): void => {
-    const win = checkWin()
-    const lose = checkLose()
+    board = createBoard(BOARD_SIZE, MINE_COUNT)
 
-    if(lose || win) {
-        boardElement.addEventListener("click", stopProp, { capture: true })
-        boardElement.addEventListener("contextmenu", stopProp, { capture: true })
-    }
+    board.forEach(row => {
+        row.forEach(tile => {
+            boardElement?.append(tile.element)
 
-    if (lose) {
-        gameEndText.textContent = "YOU LOSE!"
-        board.forEach(row => {
-            row.forEach(tile => {
-                if (tile.status === TILE_STATUSES.MARKED) {
-                    if (tile.mine) {
-                        tile.element.style.setProperty('background-color', '#F88F32')
-                    } else {
-                        // tile.element.style.setProperty('background-color', '#1AD927')
-                        tile.element.className = 'crossed-flag-container'
-                        const cross = document.createElement('div')
-                        cross.textContent = 'X'
-                        cross.className = 'cross'
-                        tile.element.removeChild(<Node> tile.element.firstChild)
-                        tile.element.appendChild(cross)
-                    }
-                } else if (tile.mine) {
-                    const img = document.createElement('img')
-                    img.src = './assets/mine.png'
-                    img.setAttribute('draggable', 'false')
-                    img.className = 'mine'
-                    tile.element.appendChild(img)
-                    tile.status = TILE_STATUSES.MINE
-                }
+            tile.element.addEventListener('click', () => {
+                revealTile(board, tile)
+                checkGameEnd()
+            })
+            tile.element.addEventListener('contextmenu', (e) => {
+                e.preventDefault()
+                markTile(tile)
+                countMinesLeft()
             })
         })
-    } else if (win) {
-        gameEndText.textContent = "YOU WIN!"
+    })
+
+    const countMinesLeft = (): void => {
+        const markedMines = board.reduce((count, row) => {
+            return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+        }, 0)
+
+        minesLeftText.textContent = MINE_COUNT - markedMines + ""
+    }
+
+    const checkGameEnd = (): void => {
+        const win = checkWin()
+        const lose = checkLose()
+
+        if (lose || win) {
+            board.forEach(row => {
+                row.forEach(tile => {
+                    tile.element.addEventListener('click', stopProp, { capture: true })
+                    tile.element.addEventListener('contextmenu', stopProp, { capture: true })
+                })
+            })
+        }
+
+        if (lose) {
+            gameEndText.textContent = "YOU LOSE!"
+            board.forEach(row => {
+                row.forEach(tile => {
+                    if (tile.status === TILE_STATUSES.MARKED) {
+                        if (tile.mine) {
+                            tile.element.style.setProperty('background-color', '#F88F32')
+                        } else {
+                            // tile.element.style.setProperty('background-color', '#1AD927')
+                            tile.element.className = 'crossed-flag-container'
+                            const cross = document.createElement('div')
+                            cross.textContent = 'X'
+                            cross.className = 'cross'
+                            tile.element.removeChild(<Node>tile.element.firstChild)
+                            tile.element.appendChild(cross)
+                        }
+                    } else if (tile.mine) {
+                        const img = document.createElement('img')
+                        img.src = './assets/mine.png'
+                        img.setAttribute('draggable', 'false')
+                        img.className = 'mine'
+                        tile.element.appendChild(img)
+                        tile.status = TILE_STATUSES.MINE
+                    }
+                })
+            })
+        } else if (win) {
+            gameEndText.textContent = "YOU WIN!"
+        }
+    }
+
+    const checkLose = () => {
+        return board.some(row => {
+            return row.some(tile => {
+                return tile.status === TILE_STATUSES.MINE
+            })
+        })
+    }
+
+    const checkWin = () => {
+        const revealedCount = board.reduce((count, row) => {
+            return count + row.reduce((count, tile) => {
+                return count + (tile.status === TILE_STATUSES.NUMBER ? 1 : 0)
+            }, 0)
+        }, 0)
+
+        return revealedCount === BOARD_SIZE * BOARD_SIZE - MINE_COUNT
+    }
+
+    const stopProp = (e: Event) => {
+        e.stopImmediatePropagation()
     }
 }
 
-const checkLose = () => {
-    return board.some(row => {
-        return row.some(tile => {
-            return tile.status === TILE_STATUSES.MINE
-        })
-    })
-}
-
-const checkWin = () => {
-    const revealedCount = board.reduce((count, row) => {
-        return count + row.reduce((count, tile) => {
-            return count + (tile.status === TILE_STATUSES.NUMBER ? 1: 0)
-        }, 0)
-    }, 0)
-
-    return revealedCount === BOARD_SIZE * BOARD_SIZE - MINE_COUNT 
-}
-
-const stopProp = (e: Event) => {
-    e.stopImmediatePropagation()
-}
+play()
